39a8291b569d3740c20e7bd70cdadcdb8193a898
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-11-01 00:36:16 +0530

    get all products(paginated) endpoint implemented

--- inventory-service/src/main/java/com/example/inventoryservice/controller/ProductController.java
+++ inventory-service/src/main/java/com/example/inventoryservice/controller/ProductController.java
@@ -1,12 +1,15 @@
 package com.example.inventoryservice.controller;
 
 import com.example.inventoryservice.dto.ProductRequestDTO;
+import com.example.inventoryservice.dto.ProductResponseDTO;
 import com.example.inventoryservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -19,5 +22,16 @@ public class ProductController {
         return ResponseEntity.ok(result);
     }
 
+    @GetMapping("/get-products")
+    public ResponseEntity<Page<ProductResponseDTO>> getAllProducts(
+            @RequestParam(name = "page", defaultValue = "0", required = false) Integer page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) Integer size
+    ) {
+
+        ResponseEntity<Page<ProductResponseDTO>> responseEntity = null;
+
+        Page<ProductResponseDTO> pageDTOs = productService.getAllProducts(page, size);
+        return responseEntity.ok(pageDTOs);
+    }
 
 }
--- inventory-service/src/main/java/com/example/inventoryservice/service/ProductService.java
+++ inventory-service/src/main/java/com/example/inventoryservice/service/ProductService.java
@@ -2,10 +2,17 @@ package com.example.inventoryservice.service;
 
 
 import com.example.inventoryservice.dto.ProductRequestDTO;
+import com.example.inventoryservice.dto.ProductResponseDTO;
 import com.example.inventoryservice.entity.Product;
 import com.example.inventoryservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -24,5 +31,29 @@ public class ProductService {
         ;
         productRepository.save(newProduct);
         return  "Product is successfully added to the inventory";
+    }
+
+    public Page<ProductResponseDTO> getAllProducts(Integer page, Integer size) {
+        PageRequest pageable = PageRequest.of(page, size);
+        Page<Product> pageEntities = productRepository.findAll(pageable);
+
+        List<Product> entityList = pageEntities.getContent();
+        List<ProductResponseDTO> dtoList = new ArrayList<>();
+
+        entityList.forEach(entity -> dtoList.add(convertToProductResponse(entity)));
+
+        return new PageImpl<>(dtoList, pageable, pageEntities.getTotalElements());
+    }
+
+    private ProductResponseDTO convertToProductResponse(Product entity) {
+        ProductResponseDTO productResponseDTO=ProductResponseDTO.builder()
+                                                                .category(entity.getCategory())
+                                                                .quantity(entity.getQuantity())
+                                                                .price(entity.getPrice())
+                                                                .id(entity.getId())
+                                                                .description(entity.getDescription())
+                                                                .imageURL(entity.getImageURL())
+                                                                .build();
+        return productResponseDTO;
     }
 }
