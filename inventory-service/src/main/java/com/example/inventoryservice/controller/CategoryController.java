29b79b2688613a371373cd67661786bb5b04e8c1
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-11-02 20:27:34 +0530

    Implemented add,remove,get-all endpoints for category

--- inventory-service/src/main/java/com/example/inventoryservice/controller/CategoryController.java
+++ inventory-service/src/main/java/com/example/inventoryservice/controller/CategoryController.java
@@ -1,10 +1,37 @@
 package com.example.inventoryservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.inventoryservice.dto.CategoryRequestDTO;
+import com.example.inventoryservice.dto.CategoryResponseDTO;
+import com.example.inventoryservice.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("/api/v1/category")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+    @PostMapping("/add-category")
+    public ResponseEntity<String> addItem(@RequestBody CategoryRequestDTO categoryRequestDTO){
+        String result= categoryService.addNewCategory(categoryRequestDTO);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("delete-category/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable(name = "id") int id){
+        String result= categoryService.deleteCategory(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("get-categories")
+    public ResponseEntity<List<CategoryResponseDTO>> getAllCategories(){
+        List<CategoryResponseDTO> categoryResponseDTOList=categoryService.getAllCategories();
+        return ResponseEntity.ok(categoryResponseDTOList);
+    }
+
 
 }
