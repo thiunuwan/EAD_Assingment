1b2f0231e2ca79ce6fdaa62b23b8a3dd80d2c864
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2023-11-03 23:10:26 +0530

    initial commit

---
+++ auth-service/src/main/java/com/thiunuwan/authservice/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.thiunuwan.authservice.service;
+
+
+import com.thiunuwan.authservice.entity.UserCredential;
+import com.thiunuwan.authservice.repository.UserCredentialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserCredentialRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtService jwtService;
+
+    public String saveUser(UserCredential credential) {
+        credential.setPassword(passwordEncoder.encode(credential.getPassword()));
+        repository.save(credential);
+        return "user added to the system";
+    }
+
+    public String generateToken(String username) {
+        return jwtService.generateToken(username);
+    }
+
+    public void validateToken(String token) {
+        jwtService.validateToken(token);
+    }
+
+
+}
