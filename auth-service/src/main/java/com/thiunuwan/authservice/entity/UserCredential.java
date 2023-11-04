a29e6f8733a1cc8d65688faf9132db71705a0b33
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2023-11-04 19:52:42 +0530

    implements jwt configuration(fixed)

--- auth-service/src/main/java/com/thiunuwan/authservice/entity/UserCredential.java
+++ auth-service/src/main/java/com/thiunuwan/authservice/entity/UserCredential.java
@@ -1,9 +1,6 @@
 package com.thiunuwan.authservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +14,9 @@ public class UserCredential {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(unique = true)
     private String username;
     private String email;
     private String password;
+
 }
