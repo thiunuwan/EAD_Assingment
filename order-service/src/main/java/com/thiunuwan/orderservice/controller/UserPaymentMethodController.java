69a898298e9e10a4ef486198252d24a12025c35c
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-11-05 18:45:05 +0530

    Create UserPaymentMethodController.java

---
+++ order-service/src/main/java/com/thiunuwan/orderservice/controller/UserPaymentMethodController.java
@@ -0,0 +1,25 @@
+package com.thiunuwan.orderservice.controller;
+
+
+//import com.thiunuwan.orderservice.entity.UserPaymentMethod;
+//import com.thiunuwan.orderservice.repository.UserPaymentMethodRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping(value = "api/v1/payment-method")
+//@CrossOrigin("*")
+//public class UserPaymentMethodController {
+//
+//    @Autowired
+//    UserPaymentMethodRepo userPaymentMethodRepo;
+//
+//    @PostMapping("/")
+//    UserPaymentMethod newUserPaymentMethod(@RequestBody UserPaymentMethod newUserPaymentMethod){
+//
+//    }
+//}
