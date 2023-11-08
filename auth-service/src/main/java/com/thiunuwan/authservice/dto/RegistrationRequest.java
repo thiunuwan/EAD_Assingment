67b0721bbdf521d34df99afc495dc23f32ed7842
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2023-11-08 12:33:46 +0530

    add registrationDTO

---
+++ auth-service/src/main/java/com/thiunuwan/authservice/dto/RegistrationRequest.java
@@ -0,0 +1,14 @@
+package com.thiunuwan.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationRequest {
+    private String username;
+    private String email;
+    private String password;
+}
